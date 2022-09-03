4c522ca5b9acfae33a00f9088c9bc6c6dda0c286
unknown <sakifur35-2749@diu.edu.bd>
2022-09-03 21:58:02 +0600

    Verify match and use function to reduce duplicate

--- app.js
+++ app.js
@@ -30,3 +30,19 @@ document.getElementById('key-pad').addEventListener('click', function (event) {
         calcInput.value = newNumber;
     }
 });
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+
+    const successMassage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        successMassage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+        successMassage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
