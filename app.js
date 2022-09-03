dcbf5e701ddfd0746cc0c0df9ab0916ee1719437
unknown <sakifur35-2749@diu.edu.bd>
2022-09-03 19:09:39 +0600

    Pin matcher overview, Generate pin and display it

---
+++ app.js
@@ -0,0 +1,16 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        console.log('got 3 digit and call again');
+        return getPin();
+    }
+}
+
+function genaratePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
